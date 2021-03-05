aa7a11ff48f2cbbea268751120d239c3fb58b45f
SachinHolalkeri <70910665+SachinHolalkeri@users.noreply.github.com>
2021-03-05 22:25:33 +0530

    Add files via upload

---
+++ Mobile Store Management System/admin/include/ft.php
@@ -0,0 +1,7 @@
+<footer id="footer" class="footer color-bg">
+	<div class="footer" style="padding-top: 20px;">
+		<div class="container">
+			<!-- <b class="copyright">&copy; Bangalore</b> Digitals -->
+		</div>
+	</div>
+</footer>
